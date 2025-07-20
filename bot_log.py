655efa442c6b8cd63952f84947c396a0c654da21
Empika1 <dravidts1@gmail.com>
2025-07-20 11:17:55 -0400

    remove redundant file open

--- bot_log.py
+++ bot_log.py
@@ -3,10 +3,7 @@ import logging
 logging.basicConfig(filename="errlog.txt", encoding="utf-8", format="%(asctime)s %(message)s", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-errlog = open('errlog.txt', "a+")
 logger.info("Logging session started.")
 
 def log_err(err: Exception):
     logger.exception(err)
-
-errlog.close()
